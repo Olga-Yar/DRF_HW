070b5a5fa34623ae93826268d8a442ac9d57f9d7
Olga Yarosh <O_ll_y_a@mail.ru>
2023-07-27 19:54:30 +0300

    создание моделей и миграция

--- study/models/course.py
+++ study/models/course.py
@@ -1,5 +1,16 @@
 from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
+
+
 class Course(models.Model):
-    pass
+    name = models.CharField(max_length=100, verbose_name='название')
+    image = models.ImageField(upload_to='Course/', verbose_name='превью', **NULLABLE)
+    about = models.TextField(verbose_name='описание', **NULLABLE)
+
+    def __str__(self):
+        return f'{self.name}: {self.about}'
+
+    class Meta:
+        verbose_name = 'курс'
+        verbose_name_plural = 'курсы'
